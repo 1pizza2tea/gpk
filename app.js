f88ecfeeb8622be80c07ca50c7020cb3b3f203e2
1pizza2tea <please1coffee@gmail.com>
2025-06-08 15:54:20 +0300

    _

--- app.js
+++ app.js
@@ -168,7 +168,7 @@ bot.catch((err) => {
     const e = err.error;
 
     if (e instanceof GrammyError) {
-      console.error('Error in request:::::::::::::::', e.description);
+      console.error('Error in request::::::::::::::', e.description);
 
     } else if (e instanceof HttpError) {
       console.error('Could not contact Telegram:', e);
